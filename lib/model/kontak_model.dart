45e8115a1e4ee5d2c7262a924b50c7c08387935d
21PAML070 <ehzand.herry1926@gmail.com>
2024-04-03 20:18:32 +0700

    menambahkan atribut dari kontak

--- lib/model/kontak_model.dart
+++ lib/model/kontak_model.dart
@@ -1,3 +1,15 @@
 class Kontak{
-  
+  final String nama;
+  final String email;
+  final String alamat;
+  final String telepon;
+  final String foto;  
+
+  Kontak({
+    required this.nama,
+    required this.email,
+    required this.alamat,
+    required this.telepon,
+    required this.foto,
+  });
 }
